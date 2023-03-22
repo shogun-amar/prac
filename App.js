f51ea7402c71dca0f27d0c67f3a81695b7671a02
shogun-amar <128471454+shogun-amar@users.noreply.github.com>
2023-03-22 20:50:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import { useState } from 'react';
 import './App.css';
 import { TodoInput } from './todoinput';
-// import { TodoList } from "./todolist";
+import { TodoList } from "./todolist";
 
 function App() {
-  const [listItems, setListItems] = useState([])
+  const [item, setItem] = useState("")
+  const [list, setList] = useState([item])
+
 
   const handleChange = (event) => {
-    setListItems(event.target.value);
+    setItem(event.target.value);
   }
   
   const handleAddItem = () =>{
-    const newListItem = listItems.push(listItems)
-    setListItems(newListItem)
+    const newItem = list.push({item})
+    setList(newItem)
   }
 
   return (
     <div className="App">
       <div>
         <h1> TodoInput </h1>
-        <TodoInput />
-        {/* <TodoList /> */}
+        <TodoInput 
+          handleAddItem = {handleAddItem}
+          handleChange = {handleChange}
+          item = {item}
+          list = {list}
+        />
+        <TodoList 
+          item = {item}
+          list = {list}
+        />
       </div>
     </div>
   );
