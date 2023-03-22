147998db1a5290d55e357012b9cedd3e4bdc8f71
shogun-amar <128471454+shogun-amar@users.noreply.github.com>
2023-03-22 20:48:43 +0530

    Update todoinput.js

--- todoinput.js
+++ todoinput.js
@@ -5,13 +5,18 @@ export const TodoInput = (props) => {
     <div>
         <h1> TodoInput </h1>
         <div>
-            <input type="text" value={props.listItems} onChange={props.handleChange} />
+            <input type="text" value={props.items} onChange={props.handleChange} />
         </div>
         <div>
             <button className='btn btn-danger' onClick={props.handleAddItem}>
                 Add New Task
             </button>
         </div>
+        <ul>
+            {props.listItems.map((item) => {
+               return item
+            })}
+        </ul>
         
     </div>
   )
